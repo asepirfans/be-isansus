a52e7a89eb687837c873cc95427f8775e03a4ada
Asep Irfan Setiawan <asepirfans.9c@gmail.com>
2024-04-14 16:07:17 +0700

    update

--- routes/sensorRoute.js
+++ routes/sensorRoute.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const sensorController = require('../controllers/sensorController');
-const wifiController = require('../controllers/wifiController');
 
 router.post('/sensor',  sensorController.createSensor),
 router.get('/sensor',  sensorController.getSensor),
